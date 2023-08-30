4c41c863168397a5ed66633d3b2718c315214ade
Demoladrew <demolasobola.ds@gmail.com>
2023-08-30 21:56:54 +0100

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+   fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -10,8 +12,14 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
 });
